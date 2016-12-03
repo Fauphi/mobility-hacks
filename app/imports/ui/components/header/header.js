c4e89c70ba76d584338d718b9662e0deac722fad
Radu <radu@attic-studio.net>
2016-12-03 21:25:10 +0100

    - working helpers for delay and custom settings for time steps

--- app/imports/ui/components/header/header.js
+++ app/imports/ui/components/header/header.js
@@ -2,7 +2,7 @@
 * @Author: Philipp
 * @Date:   2016-10-05 16:32:13
 * @Last Modified by:   Radu Gota (radu@attic-studio.net)
-* @Last Modified time: 2016-12-03 19:12:10
+* @Last Modified time: 2016-12-03 21:23:55
 */
 
 import { Template } from 'meteor/templating';
@@ -14,10 +14,12 @@ Template.header.onCreated(function created() {
 	// Session.set("statusColor", "green");
 	// Session.set("headerHeight", "");
 	Session.set("delay", {"status": true, "minutes": 2});
+	Session.set("pastStep", "0");
 	// 
 	Tracker.autorun(function() {
 	  var routeName = FlowRouter.getRouteName();
 	  console.log("Current route name is: ", routeName);
+	  
 	});
 });
 
@@ -33,6 +35,34 @@ Template.header.helpers({
 	timeData: function(){
 		return Session.get('timeData');
 	},
+	nextTime: function(){
+		var current = new Date().getTime();
+		// var current = Math.floor(Date.now() / 1000);
+		var next = Session.get('timeData');
+
+		if(next){
+			var i = 0;
+			if(next[i].timestamp > current){
+				var minuteCount = Math.round((next[i].timestamp - current) / 60000) % 60;
+				Session.set("minuteCount", minuteCount);
+				return next[i];
+			} else {
+				i++
+			}
+		}
+		
+	},
+	// shrinks main minute display to fit screen size
+	longTime: function(){
+		// console.log();
+		if (this.toFixed() > 9){
+			return true;
+		}
+	},
+	// minute count for main display
+	minuteCount: function(){
+		return Session.get("minuteCount");
+	},
 	isMain: function(){
 		let routeName = FlowRouter.getRouteName();
 		if(routeName == "main"){
@@ -45,8 +75,27 @@ Template.header.helpers({
 			return true;
 		}
 	},
-	delay: function(){
-		return Session.get("delay");
+	hasDelay: function(){
+		if(this.delay > 0){
+			return true;
+		}
+	},
+	timeSteps: function(){
+		var selected = this.timestamp;
+		var data = Session.get('timeData');
+		if(data){
+			for(i = 0; i < data.length; i++){
+				var x = i + 1
+				,	diff = Math.round((data[x].timestamp - data[i].timestamp) / 60000) % 60
+				if( diff > 20){
+					return "large";
+				} else if(diff < 10){
+					return "small"
+				} else {
+					return "medium"
+				};
+			}
+		}			
 	}
 });
 
